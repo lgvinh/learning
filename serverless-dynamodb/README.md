80329e3873cd8409ea1128402d729414f14b0a1c
vinh.lam <vinh.lam@orientsoftware.com>
2020-12-01 19:31:30 +0700

    new modifieD

---
+++ serverless-dynamodb/README.md
@@ -0,0 +1,2 @@
+- npm i serverless -g
+- install python
---
+++ serverless-dynamodb/handler.js
@@ -0,0 +1,28 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+  region: "localhost",
+  endpoint: "http://localhost:8042",
+});
+
+module.exports.hello = async (event, context) => {
+  const test = await docClient.scan({
+    TableName: "petShopApp",
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        test,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
